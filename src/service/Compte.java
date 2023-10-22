a7a32ce4be6d5c2737c7d4014ca46c9e7cb44954
siwarsayar <siwarsayar@gmail.com>
2023-10-22 20:13:26 +0100

    COMMIT

--- src/service/Compte.java
+++ src/service/Compte.java
@@ -1,5 +1,39 @@
 package service;
 
 public class Compte {
+	 private int code;
+	    private int solde;
+	    private String dateCreation;
+
+	    public Compte(int code, int solde, String dateCreation) {
+	        super();
+	        this.code = code;
+	        this.solde = solde;
+	        this.dateCreation = dateCreation;
+	    }
+
+	    public int getCode() {
+	        return code;
+	    }
+
+	    public void setCode(int code) {
+	        this.code = code;
+	    }
+
+	    public int getSolde() {
+	        return solde;
+	    }
+
+	    public void setSolde(int solde) {
+	        this.solde = solde;
+	    }
+
+	    public String getDateCreation() {
+	        return dateCreation;
+	    }
+
+	    public void setDateCreation(String dateCreation) {
+	        this.dateCreation = dateCreation;
+	    }
 
 }
