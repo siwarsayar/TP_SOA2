275b77e3b88b14c45b193c20607fadc87614a3a5
siwarsayar <siwarsayar@gmail.com>
2023-10-27 12:40:38 +0100

    tp2soa

--- src/service/Main.java
+++ src/service/Main.java
@@ -1,12 +1,30 @@
 package service;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 BanqueService b = new BanqueService();
-	        b.conversion(360);
+         BanqueService ser =new BanqueService();
+		
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("donner un montant:\n");
+			double TND =scan.nextDouble();
+			System.out.println("le montant avant le conversion ="+TND);
+			
+			double  Euro =ser.conversion(TND);
 
+			System.out.println("\nle montant aprés le conversion ="+Euro);
+		}
+		ser.addCompte(new Compte(99,20.500,"09/03/2023" ));
+		ser.addCompte(new Compte(100,100000.0,"14/06/2023"));
+		ser.addCompte(new Compte(200,70000.6,"09/10/2023"));
+		
+		
+		
+		
+		
+		ser.toString();
+		
 	}
-
 }
